34d2aa1a927ff50dac622e4cf2f91e2e7d344162
Paul Rose <u9d91@csr62-scam.(none)>
2011-11-15 15:58:13 +0000

    End of uni day commit 15 Niv 11

---
+++ src/me/paulrose/lptc/Window.java
@@ -0,0 +1,5 @@
+package me.paulrose.lptc;
+
+public class Window {
+
+}
---
+++ src/me/paulrose/lptc/editor/Editor.java
@@ -0,0 +1,13 @@
+package me.paulrose.lptc.editor;
+
+import javax.swing.JPanel;
+
+public class Editor extends JPanel{
+	
+	
+	
+	public Editor(){
+		
+	}
+
+}
